75a11822a60bf7f1dc410490980561ffcdb306cb
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-03-23 01:17:25 +0900

    test: server test

--- src/Pages/MainPage/MainAbout.js
+++ src/Pages/MainPage/MainAbout.js
@@ -1,39 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "./Components/Carousel";
 import axios from "axios";
 import styled from "styled-components";
 
-
 const MainAbout = () => {
-    const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        axios.get('https://port-0-promoationpage-server-12fhqa2blnlum4de.sel5.cloudtype.app/api/projects')
-            .then((response) => {
-                const data = response.data;
-                const urlList = [];
+  const fetchData = () => {
+    axios
+      .get("http://3.36.117.230:8080/api/projects")
+      .then((response) => {
+        const data = response.data;
+        const urlList = [];
 
-                for(let i = 0; i < data.data.length; i++){
-                    //썸네일만 가져오기
-                    urlList.push(data.data[i].imageUrlList[0]);
-                }
+        for (let i = 0; i < data.data.length; i++) {
+          //썸네일만 가져오기
+          urlList.push(data.data[i].imageUrlList[0]);
+        }
 
-                setImages(urlList);
+        setImages(urlList);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-            }).catch((error) => {
-                console.error(error);
-        })
-    }
-
-    return (
-
-        <Carousel images={images}></Carousel>
-
-    );
-}
+  return <Carousel images={images}></Carousel>;
+};
 
 export default MainAbout;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,16 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware('/api', {
-            target: "https://port-0-promoationpage-server-12fhqa2blnlum4de.sel5.cloudtype.app/",
-    changeOrigin: true,
-})
-);
-    app.use(
-        createProxyMiddleware( '/user-service',{
-            target: 'http://13.125.181.139:8000',
-            changeOrigin: true
-        })
-    );
-
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "http://3.36.117.230:8080/",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    createProxyMiddleware("/user-service", {
+      //   target: "http://13.125.181.139:8000",
+      changeOrigin: true,
+    })
+  );
 };
