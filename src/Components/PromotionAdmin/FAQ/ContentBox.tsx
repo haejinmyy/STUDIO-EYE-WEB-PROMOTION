ecfe40f12ae4bd656c21d1c8c3ad93a3299737e0
kimgoat <kimgoatlove@gmail.com>
2024-04-11 20:19:41 +0900

    chore: 스타일 코드 수정 및 파일 삭제

--- src/Components/PromotionAdmin/FAQ/ContentBox.tsx
+++ src/Components/PromotionAdmin/FAQ/ContentBox.tsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
 
 export const ContentBox = styled.div`
-  position: fixed;
-  left: 10rem;
-  top: 8rem;
+  position: relative;
   border-radius: 1rem;
   background-color: ${(props) => props.theme.color.white.bold};
   border: 0.5px solid ${(props) => props.theme.color.black.light};
-  width: 700px;
-  height: 80%;
+  width: 40vw;
+  height: 80vh;
 `;
