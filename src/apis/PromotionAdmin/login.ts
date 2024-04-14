97ca11f66d0e473338809a4df4b25eee498b63bf
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-04-14 23:44:04 +0900

    fix: end point

--- src/apis/PromotionAdmin/login.ts
+++ src/apis/PromotionAdmin/login.ts
@@ -1,8 +1,6 @@
 import { loginType } from '@/types/PromotionAdmin/login';
 import axios from 'axios';
 
-const BASE_URL = 'http://43.201.95.252:8000';
-
 export const login = async (email: string, pwd: string): Promise<loginType> => {
   try {
     const response = await axios.post('/user-service/login', { email, pwd });
