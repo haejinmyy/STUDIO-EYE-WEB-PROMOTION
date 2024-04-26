1152ca5f0cbf13e31382c9438b3f28646daf56f5
kimgoat <kimgoatlove@gmail.com>
2024-04-26 19:15:11 +0900

    chore: theme 색상 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,9 +2,11 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
   color: {
+    symbol: '#FFA900',
+    background: '#000000',
     black: {
       bold: '#141414',
-      light: '#878787',
+      light: '#8A8A8A',
       pale: '#C4C4C4',
     },
     white: {
