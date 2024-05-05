7542a1436c73682a895906c6f596a08f53bf2cad
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-05-05 09:42:12 +0900

    etc: etc

--- src/Router.tsx
+++ src/Router.tsx
@@ -31,6 +31,7 @@ import ClientPage from './pages/PromotionAdmin/DataEditPage/ClientPage/ClientPag
 import PartnerEditPage from './pages/PromotionAdmin/DataEditPage/PartnerPage/PartnerEditPage';
 import ClientEditPage from './pages/PromotionAdmin/DataEditPage/ClientPage/ClientEditPage';
 import PartnerWritePage from './pages/PromotionAdmin/DataEditPage/PartnerPage/PartnerWritePage';
+import FaqPage from './pages/PromotionPage/FaqPage/FaqPage';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -58,7 +59,10 @@ const router = createBrowserRouter([
             path: PP_ROUTES_CHILD.ARTWORK_DETAIL,
             element: <ArtworkDetailPage />,
           },
-
+          {
+            path: PP_ROUTES_CHILD.FAQ,
+            element: <FaqPage />,
+          },
           {
             path: PP_ROUTES_CHILD.CONTACT,
             element: <ContactUsPage />,
