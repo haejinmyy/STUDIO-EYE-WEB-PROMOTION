68b35d442d892ac9b2e2686ee733633f79ed8841
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-05-07 03:26:11 +0900

    build: build

--- src/pages/PromotionAdmin/Login/Login.tsx
+++ src/pages/PromotionAdmin/Login/Login.tsx
@@ -19,10 +19,14 @@ const Login = () => {
       const formData = { email, pwd };
       console.log('보내는 데이터', formData);
       const response = await login(formData);
-      setAuth({ accessToken: response.accessToken, userId: response.id });
-      window.alert('로그인 성공');
-      if (auth.userId) {
-        navigate(PA_ROUTES.HOME);
+
+      if (response.approved) {
+        setAuth({ accessToken: response.accessToken, userId: response.id });
+        window.alert('로그인 성공');
+        navigate(PA_ROUTES.HOME); // approved가 true일 경우에만 홈으로 이동합니다.
+      } else {
+        // approved가 false일 경우
+        throw new Error('승인되지 않은 계정입니다.');
       }
     } catch (error) {
       const errorMessage = '로그인에 실패했습니다. 다시 시도해주세요.';
