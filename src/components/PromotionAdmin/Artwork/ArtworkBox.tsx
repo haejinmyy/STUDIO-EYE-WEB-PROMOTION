14be2076d9c8ad780a2fd879361b7e6a03163e30
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-05-07 01:55:47 +0900

    feat: artwork name search, design

--- src/components/PromotionAdmin/Artwork/ArtworkBox.tsx
+++ src/components/PromotionAdmin/Artwork/ArtworkBox.tsx
@@ -2,24 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as IsPosted } from '@/assets/images/isPosted.svg';
 import { ReactComponent as IsNotPosted } from '@/assets/images/isNotPosted.svg';
+import { ArtworkData } from '@/types/PromotionAdmin/artwork';
 
-type Props = {
-  mainImg: string;
-  clientName: string;
-  name: string;
-  isPosted: boolean;
-};
-
-const ArtworkBox = ({ mainImg, clientName, name, isPosted }: Props) => {
+const ArtworkBox = ({
+  id,
+  department,
+  category,
+  name,
+  client,
+  date,
+  link,
+  overView,
+  projectType,
+  isPosted,
+  mainImg,
+  projectImages,
+}: ArtworkData) => {
   return (
     <Container>
       {mainImg ? <img src={mainImg} alt='mainImg' /> : <NoMainImageWrapper>No Image</NoMainImageWrapper>}
       <DescriptionWrapper>
-        <div>
-          <h2>{clientName}</h2>
-          <h1>{name}</h1>
-        </div>
-        {isPosted ? <IsPosted /> : <IsNotPosted />}
+        <Wrapper>
+          <div>
+            <h2>{client}</h2>
+            <h1>{name}</h1>
+          </div>
+          <RightAlignWrapper>
+            {isPosted ? <IsPosted /> : <IsNotPosted />}
+            <h2>{category}</h2>
+          </RightAlignWrapper>
+        </Wrapper>
+        <h3>{overView}</h3> <TypeWrapper projectType={projectType}>{projectType}</TypeWrapper>
       </DescriptionWrapper>
     </Container>
   );
@@ -29,19 +42,22 @@ export default ArtworkBox;
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 180px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
   border-radius: 10px;
-  background-color: #dbdbdb14;
-  padding: 8px;
+  background-color: #afafaf13;
+  padding: 20px;
+  box-sizing: border-box;
+
   &:hover {
     cursor: pointer;
     opacity: 0.8;
     transition: all ease-in-out 300ms;
   }
   img {
-    width: 180px;
-    height: 180px;
+    width: 130px;
+    height: 130px;
     object-fit: cover;
     border-radius: 5px;
   }
@@ -56,12 +72,19 @@ const Container = styled.div`
     font-size: 15px;
     color: #707070;
   }
+  h3 {
+    font-family: 'pretendard-medium';
+    font-size: 15px;
+    color: #4b4b4b;
+    line-height: 18px;
+  }
 `;
 
-const DescriptionWrapper = styled.div`
-  margin-top: 10px;
+const Wrapper = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
+  margin-bottom: 23px;
 `;
 
 const NoMainImageWrapper = styled.div`
@@ -70,4 +93,28 @@ const NoMainImageWrapper = styled.div`
   font-family: 'pretendard-medium';
   font-size: 15px;
   color: #ffffff;
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 23px;
+`;
+const RightAlignWrapper = styled.div`
+  text-align: right;
+`;
+
+const TypeWrapper = styled.div<{ projectType: 'others' | 'top' | 'main' }>`
+  width: fit-content;
+  height: fit-content;
+  padding: 5px 10px;
+  border-radius: 10px;
+  background-color: ${({ projectType }) =>
+    projectType === 'main' ? '#ffaa007d' : projectType === 'top' ? '#d3002384' : '#33333321'};
+  margin-bottom: 5px;
+  margin-left: auto;
+  font-family: 'pretendard-medium';
+  font-size: 15px;
+  color: ${({ projectType }) => (projectType === 'main' || projectType === 'top' ? 'white' : 'black')};
 `;
