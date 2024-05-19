d1166b811834d6603175ceedf6fde7bdba65f4df
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-05-19 23:24:25 +0900

    feat: 기본 meta tag 추가, google search console 추가

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
-  return <Outlet />;
+  return (
+    <Helmet>
+      <title>스튜디오아이</title>
+      <Outlet />
+    </Helmet>
+  );
 }
--- src/components/helmets/DetailHelments.tsx
+++ src/components/helmets/DetailHelments.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const DetailHelments = () => {
-  return <div>DetailHelments</div>;
+type Props = {
+  title: string;
+  description?: string;
+  keywords?: string;
+};
+
+const DetailHelments = ({ title, description, keywords }: Props) => {
+  return (
+    <>
+      <title>{title}</title>
+      {description && <meta name='description' content={description} />}
+      {keywords && <meta name='keywords' content={keywords} />}
+    </>
+  );
 };
 
 export default DetailHelments;
