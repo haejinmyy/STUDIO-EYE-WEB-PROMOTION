520f34ac79888269d6da96d6abcfe2cc1a811475
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-06-03 15:49:31 +0900

    fix: graph 소수점 안나오게 수정

--- src/components/PromotionAdmin/Home/Graph/LineGraph.tsx
+++ src/components/PromotionAdmin/Home/Graph/LineGraph.tsx
@@ -23,7 +23,7 @@ const LineGraph = ({ data, division }: LineGraphProps) => {
           stacked: true,
           reverse: false,
         }}
-        yFormat=' >-.2f'
+        yFormat={(value) => Math.round(Number(value)).toString()}
         curve='linear'
         axisTop={null}
         axisRight={null}
@@ -44,6 +44,8 @@ const LineGraph = ({ data, division }: LineGraphProps) => {
           legendOffset: -40,
           legendPosition: 'middle',
           truncateTickAt: 0,
+          tickValues: 5,
+          format: (value) => Math.round(value).toString(),
         }}
         enablePoints={true}
         pointSize={10}
