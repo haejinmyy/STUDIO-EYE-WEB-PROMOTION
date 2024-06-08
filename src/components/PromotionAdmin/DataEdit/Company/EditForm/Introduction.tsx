039ce6041b06c9412c9e619c7ee1b9b1c063ce6a
kimgoat <kimgoatlove@gmail.com>
2024-06-08 16:55:27 +0900

    feat: company introduction 글자수 제한

--- src/components/PromotionAdmin/DataEdit/Company/EditForm/Introduction.tsx
+++ src/components/PromotionAdmin/DataEdit/Company/EditForm/Introduction.tsx
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import htmlToDraft from 'html-to-draftjs';
-import { IEditorData } from '@/types/PromotionAdmin/faq';
-import TextColorEditor from '../../TextColorEditor';
 import { useQuery } from 'react-query';
 import { getCompanyData } from '@/apis/PromotionAdmin/dataEdit';
 import { ICompanyData } from '@/types/PromotionAdmin/dataEdit';
-
 import { Wrapper, ContentBlock, InputWrapper, InputTitle } from '../CompanyFormStyleComponents';
 import axios from 'axios';
 import { PROMOTION_BASIC_PATH } from '@/constants/basicPathConstants';
-import draftToHtml from 'draftjs-to-html';
-import { DATAEDIT_NOTICE_COMPONENTS, DATAEDIT_TITLES_COMPONENTS } from '../StyleComponents';
+import { DATAEDIT_NOTICE_COMPONENTS, DATAEDIT_TITLES_COMPONENTS, INPUT_MAX_LENGTH } from '../StyleComponents';
 import Button from '../../StyleComponents/Button';
 import styled from 'styled-components';
 import { MSG } from '@/constants/messages';
+import TextColorEditor from '@/components/TextEditor/TextColorEditor';
 
 interface IIntrodutionProps {
   setEditIntroduction: (editMode: boolean) => void;
@@ -24,9 +18,9 @@ interface IIntrodutionProps {
 const Introduction = ({ setEditIntroduction }: IIntrodutionProps) => {
   const { data, isLoading, error } = useQuery<ICompanyData, Error>(['company', 'id'], getCompanyData);
   const [putData, setPutData] = useState({
-    mainOverview: data?.mainOverview,
-    commitment: data?.commitment,
-    introduction: data?.introduction,
+    mainOverview: data?.mainOverview || '',
+    commitment: data?.commitment || '',
+    introduction: data?.introduction || '',
   });
 
   useEffect(() => {
@@ -36,62 +30,25 @@ const Introduction = ({ setEditIntroduction }: IIntrodutionProps) => {
         commitment: data.commitment,
         introduction: data.introduction,
       });
-
-      setMainOverviewState(() => {
-        const blocksFromHtml = htmlToDraft(data.mainOverview);
-        if (blocksFromHtml) {
-          const { contentBlocks, entityMap } = blocksFromHtml;
-          const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-          return EditorState.createWithContent(contentState);
-        } else {
-          return EditorState.createEmpty();
-        }
-      });
-
-      setCommitmentState(() => {
-        const blocksFromHtml = htmlToDraft(data.commitment);
-        if (blocksFromHtml) {
-          const { contentBlocks, entityMap } = blocksFromHtml;
-          const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-          return EditorState.createWithContent(contentState);
-        } else {
-          return EditorState.createEmpty();
-        }
-      });
-
-      setIntroductionState(() => {
-        const blocksFromHtml = htmlToDraft(data.introduction);
-        if (blocksFromHtml) {
-          const { contentBlocks, entityMap } = blocksFromHtml;
-          const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-          return EditorState.createWithContent(contentState);
-        } else {
-          return EditorState.createEmpty();
-        }
-      });
     }
   }, [data]);
 
-  const [mainOverviewState, setMainOverviewState] = useState(EditorState.createEmpty());
-  const [commitmentState, setCommitmentState] = useState(EditorState.createEmpty());
-  const [introductionState, setIntroductionState] = useState(EditorState.createEmpty());
-  const [blocks, setBlocks] = useState<IEditorData[]>([]);
-  const updateMainOverview = async (state: EditorState) => {
-    await setMainOverviewState(state);
-    setBlocks(convertToRaw(mainOverviewState.getCurrentContent()).blocks);
+  const [mainOverviewState, setMainOverviewState] = useState(data?.mainOverview || '');
+  const [commitmentState, setCommitmentState] = useState(data?.commitment || '');
+  const [introductionState, setIntroductionState] = useState(data?.introduction || '');
+
+  const updateMainOverview = (state: string) => {
+    setMainOverviewState(state);
   };
-  const updateCommitment = async (state: EditorState) => {
-    await setCommitmentState(state);
-    setBlocks(convertToRaw(commitmentState.getCurrentContent()).blocks);
+  const updateCommitment = (state: string) => {
+    setCommitmentState(state);
   };
-  const updateIntroduction = async (state: EditorState) => {
-    await setIntroductionState(state);
-    setBlocks(convertToRaw(introductionState.getCurrentContent()).blocks);
+  const updateIntroduction = (state: string) => {
+    setIntroductionState(state);
   };
 
-  const checkIsEmpty = (editorState: EditorState, attribute: string) => {
-    const isEmpty = !editorState.getCurrentContent().hasText();
-    if (isEmpty) {
+  const checkIsEmpty = (text: string, attribute: string) => {
+    if (!text.trim()) {
       alert(`${attribute}을(를) 작성해주세요.`);
       return true;
     }
@@ -100,9 +57,9 @@ const Introduction = ({ setEditIntroduction }: IIntrodutionProps) => {
 
   const handleSaveClick = async () => {
     const updateData = {
-      mainOverview: draftToHtml(convertToRaw(mainOverviewState.getCurrentContent())),
-      commitment: draftToHtml(convertToRaw(commitmentState.getCurrentContent())),
-      introduction: draftToHtml(convertToRaw(introductionState.getCurrentContent())),
+      mainOverview: mainOverviewState,
+      commitment: commitmentState,
+      introduction: introductionState,
     };
 
     const isEmpty =
@@ -141,18 +98,21 @@ const Introduction = ({ setEditIntroduction }: IIntrodutionProps) => {
             editorState={mainOverviewState}
             onEditorStateChange={updateMainOverview}
             attribute='Main Overview'
+            charLimit={INPUT_MAX_LENGTH.INFOMATION_MAIN_OVERVIEW}
           />
           <InputTitle>Commitment</InputTitle>
           <TextColorEditor
             editorState={commitmentState}
             onEditorStateChange={updateCommitment}
             attribute='Commitment'
+            charLimit={INPUT_MAX_LENGTH.INFOMATION_COMMITMENT}
           />
           <InputTitle>Introduction</InputTitle>
           <TextColorEditor
             editorState={introductionState}
             onEditorStateChange={updateIntroduction}
             attribute='Introduction'
+            charLimit={INPUT_MAX_LENGTH.INFOMATION_INTRODUCTION}
           />
         </InputWrapper>
       </ContentBlock>
